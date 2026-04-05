e2b45de6c5acf4c517768d9d39e4c426a6f71061
mqbaka02 <mqbaka@gmail.com>
2026-04-05 18:45:23 +0300

    Set cookie when login; Users page;

--- socket-for-chat/app/admin/page.tsx
+++ socket-for-chat/app/admin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -14,6 +16,12 @@ export default function Page() {
         <div className="flex flex-col gap-4 items-center justify-center">
             <div className="text-xl">
                 This is the admin page.<br/>No one, I tel you, no one is allowed here but the admin.
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+                <Link href={"/admin/users"} className="flex justify-between bg-blue-700 text-white py-2 px-4 opacity-80 hover:opacity-100 transition-opacity duration-300 group" >Users <ChevronRightIcon className="size-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300" /></Link>
+                <Link href={"/admin/users"} className="flex justify-between bg-blue-700 text-white py-2 px-4 opacity-80 hover:opacity-100 transition-opacity duration-300 group" >Users <ChevronRightIcon className="size-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300" /></Link>
+                <Link href={"/admin/users"} className="flex justify-between bg-blue-700 text-white py-2 px-4 opacity-80 hover:opacity-100 transition-opacity duration-300 group" >Users <ChevronRightIcon className="size-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300" /></Link>
+                <Link href={"/admin/users"} className="flex justify-between bg-blue-700 text-white py-2 px-4 opacity-80 hover:opacity-100 transition-opacity duration-300 group" >Users <ChevronRightIcon className="size-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300" /></Link>
             </div>
         </div>
     </>;
---
+++ socket-for-chat/app/admin/users/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return <>
+        <div className="flex flex-col gap-4 items-center justify-center">
+            <div className="text-xl">
+                Users Page
+            </div>
+        </div>
+    </>;
+};
